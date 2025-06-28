eb6a6c666c71b88311216bec1bd216ad6a909193
Aniket <161963782+Aniket8023@users.noreply.github.com>
2025-06-28 22:46:46 +0530

    Update AuthorService.java

--- BookStore/src/main/java/com/bookstore/service/AuthorService.java
+++ BookStore/src/main/java/com/bookstore/service/AuthorService.java
@@ -3,7 +3,23 @@ package com.bookstore.service;
 import com.bookstore.entity.Author;
 import java.util.List;
 
+/**
+ * Service interface for managing authors.
+ */
 public interface AuthorService {
+
+    /**
+     * Retrieves all authors.
+     *
+     * @return list of authors
+     */
     List<Author> getAllAuthors();
+
+    /**
+     * Adds a new author.
+     *
+     * @param author the author to be added
+     * @return saved author
+     */
     Author addAuthor(Author author);
 }
