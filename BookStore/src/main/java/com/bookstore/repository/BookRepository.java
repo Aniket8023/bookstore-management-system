a97b02083150175d0346a86d2fc35b1da2a9ee26
Aniket <161963782+Aniket8023@users.noreply.github.com>
2025-06-28 22:46:12 +0530

    Update BookRepository.java

--- BookStore/src/main/java/com/bookstore/repository/BookRepository.java
+++ BookStore/src/main/java/com/bookstore/repository/BookRepository.java
@@ -2,5 +2,13 @@ package com.bookstore.repository;
 
 import com.bookstore.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {}
+/**
+ * Repository interface for Book entity.
+ * Provides CRUD operations using Spring Data JPA.
+ */
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    // Additional custom queries (if any) can be added here
+}
