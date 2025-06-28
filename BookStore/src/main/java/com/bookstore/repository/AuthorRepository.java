0f776f7957c6743a7d94f249cb0bbf6c498ed35b
Aniket <161963782+Aniket8023@users.noreply.github.com>
2025-06-28 22:45:44 +0530

    Update AuthorRepository.java

--- BookStore/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ BookStore/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -4,7 +4,11 @@ import com.bookstore.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for Author entity.
+ * Provides CRUD operations using Spring Data JPA.
+ */
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    // Additional custom queries (if any) can be added here
 }
-
