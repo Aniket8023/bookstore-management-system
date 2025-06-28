0687c5759fe09379634eedc0cadf94ef9aff15ce
Aniket <161963782+Aniket8023@users.noreply.github.com>
2025-06-28 22:40:16 +0530

    Update JpaConfig.java

--- BookStore/src/main/java/com/bookstore/config/JpaConfig.java
+++ BookStore/src/main/java/com/bookstore/config/JpaConfig.java
@@ -1,6 +1,5 @@
 package com.bookstore.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -9,11 +8,20 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
-
+/**
+ * JPA Configuration class for setting up EntityManagerFactory.
+ */
 @Configuration
 @EnableTransactionManagement
 public class JpaConfig {
 
+    /**
+     * Configures the EntityManagerFactory bean.
+     *
+     * @param dataSource       injected data source
+     * @param jpaVendorAdapter JPA vendor adapter (Hibernate)
+     * @return entity manager factory bean
+     */
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(
             DataSource dataSource,
@@ -22,8 +30,7 @@ public class JpaConfig {
         LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
         factoryBean.setDataSource(dataSource);
         factoryBean.setJpaVendorAdapter(jpaVendorAdapter);
-        factoryBean.setPackagesToScan("com.bookstore");
+        factoryBean.setPackagesToScan("com.bookstore"); // Scan entity classes
         return factoryBean;
     }
 }
-
