845327a1afba1a43072edae1ad79db35fcf8a9aa
Aniket <161963782+Aniket8023@users.noreply.github.com>
2025-06-28 22:47:13 +0530

    Update AuthorServiceImpl.java

--- BookStore/src/main/java/com/bookstore/service/AuthorServiceImpl.java
+++ BookStore/src/main/java/com/bookstore/service/AuthorServiceImpl.java
@@ -4,18 +4,29 @@ import com.bookstore.entity.Author;
 import com.bookstore.repository.AuthorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+/**
+ * Implementation of AuthorService.
+ */
 @RequiredArgsConstructor
 @Service
 public class AuthorServiceImpl implements AuthorService {
+
     private final AuthorRepository authorRepository;
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public List<Author> getAllAuthors() {
         return authorRepository.findAll();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Author addAuthor(Author author) {
         return authorRepository.save(author);
