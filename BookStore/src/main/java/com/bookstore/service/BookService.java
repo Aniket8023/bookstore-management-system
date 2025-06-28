b50e20598ff87f9102d9d09ff1da6cf8ed849305
Aniket <161963782+Aniket8023@users.noreply.github.com>
2025-06-28 22:47:44 +0530

    Update BookService.java

--- BookStore/src/main/java/com/bookstore/service/BookService.java
+++ BookStore/src/main/java/com/bookstore/service/BookService.java
@@ -3,10 +3,47 @@ package com.bookstore.service;
 import com.bookstore.entity.Book;
 import java.util.List;
 
+/**
+ * Service interface for managing books.
+ */
 public interface BookService {
+
+    /**
+     * Retrieves all books.
+     *
+     * @return list of all books
+     */
     List<Book> getAllBooks();
+
+    /**
+     * Retrieves a book by its ID.
+     *
+     * @param id the ID of the book
+     * @return the book with given ID
+     */
     Book getBookById(Long id);
+
+    /**
+     * Adds a new book to the database.
+     *
+     * @param book the book to be added
+     * @return the saved book
+     */
     Book addBook(Book book);
+
+    /**
+     * Updates an existing book.
+     *
+     * @param id    ID of the book to update
+     * @param book  new book details
+     * @return updated book
+     */
     Book updateBook(Long id, Book book);
+
+    /**
+     * Deletes a book by ID.
+     *
+     * @param id the ID of the book to delete
+     */
     void deleteBook(Long id);
 }
