53abae39dc581f7cc61d474ffa08de1f6744fbed
Aniket <161963782+Aniket8023@users.noreply.github.com>
2025-06-28 22:44:11 +0530

    Update Author.java

--- BookStore/src/main/java/com/bookstore/entity/Author.java
+++ BookStore/src/main/java/com/bookstore/entity/Author.java
@@ -2,19 +2,41 @@ package com.bookstore.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+
 import java.time.LocalDate;
 
+/**
+ * Represents an Author entity in the bookstore.
+ * Contains author details like name, bio, and date of birth.
+ */
 @Entity
+@Table(name = "author")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Author {
+
+    /**
+     * Unique identifier for the author.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    /**
+     * Full name of the author.
+     */
     private String name;
+
+    /**
+     * Short biography of the author.
+     */
     private String bio;
+
+    /**
+     * Date of birth of the author.
+     */
     @Column(name = "date_of_birth")
     private LocalDate dateOfBirth;
 }
